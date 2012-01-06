e19cc7b79f8a71d92389c831ccde5666d560d76e
Steven Thomas <sthomas314@gmail.com>
2012-01-06 15:23:43 -0800

    Moved build output to 'out' directory

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ ndk=`echo $* | args.py ndk setting=android-ndk-lab126 required=1`
 android_src=`echo $* | args.py fire setting=fire required=1`
 export PATH=$PATH:$ndk/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/
 android_product=`echo $* | args.py android-product default=blaze`
+out_dir=out
 
 cc_prog=arm-linux-androideabi-gcc
 cpp_prog=arm-linux-androideabi-g++
@@ -28,14 +29,16 @@ libs+=" $android_src/out/target/product/$android_product/symbols/system/lib/libc
 libs+=" $ndk/sources/cxx-stl/stlport/libs/armeabi-v7a/libstlport_static.a"
 compiler_opts="-fno-rtti -fno-exceptions -Wno-multichar"
 
-cmd="$cpp_prog -g host.cpp common.cpp -o host $c_defs $compiler_opts $inc_dirs $libs"
+mkdir -p $out_dir
+
+cmd="$cpp_prog -g host.cpp common.cpp -o $out_dir/host $c_defs $compiler_opts $inc_dirs $libs"
 echo $cmd
 $cmd
 
-cmd="$cpp_prog -g renderer.cpp common.cpp -o renderer $c_defs $compiler_opts $inc_dirs $libs"
+cmd="$cpp_prog -g renderer.cpp common.cpp -o $out_dir/renderer $c_defs $compiler_opts $inc_dirs $libs"
 echo $cmd
 $cmd
 
 echo Copying to device
-adb push host /data/local/banshee
-adb push renderer /data/local/banshee
+adb push $out_dir/host /data/local/banshee
+adb push $out_dir/renderer /data/local/banshee
